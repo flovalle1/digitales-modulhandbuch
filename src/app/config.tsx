5cb2c7f24892566828d94084815b812c7bf188eb
Florian Martel <hello@florianmartel.de>
2024-11-17 16:56:01 +0100

    feat: complete config for all fieldOfStudies

---
+++ src/app/config.tsx
@@ -0,0 +1,175 @@
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+
+export type FieldOfStudy = {
+    id: string,
+    content: GridColDef[]
+}
+
+export const getFieldOfStudy = (id: String) => {
+    switch (id) {
+        case "cs":
+            return cs;
+        case "bio-cs":
+            return bio_cs;
+        case "media-cs":
+            return media_cs;
+        case "medic-cs":
+            return medic_cs;
+        default:
+            return cs;
+    }
+}
+
+export const cs = {
+    id: "Informatik",
+    content: [
+        {
+            field: 'mandatory',
+            headerName: 'Pflichtbereich',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'practical_cs',
+            headerName: 'Praktische Informatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'theoretical_cs',
+            headerName: 'Theoretische Informatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'technical_cs',
+            headerName: 'Technische Informatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'proseminar',
+            headerName: 'Proseminar',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        }
+    ]
+}
+
+export const bio_cs = {
+    id: "Bioinformatik",
+    content: [
+        {
+            field: 'mandatory',
+            headerName: 'Pflichtbereich',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'bioinfo',
+            headerName: 'Wahlpflicht Bioinformatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'info',
+            headerName: 'Wahlpflicht Informatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+    ]
+}
+
+export const media_cs = {
+    id: "Medieninformatik",
+    content: [
+        {
+            field: 'mandatory',
+            headerName: 'Pflichtbereich',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'mediascience',
+            headerName: 'Wahlpflicht Medienwissenschaften',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'mediainfo',
+            headerName: 'Wahlpflicht Medieninformatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'info',
+            headerName: 'Wahlpflicht Informatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+    ]
+}
+
+export const medic_cs = {
+    id: "Medizininformatik",
+    content: [
+        {
+            field: 'mandatory',
+            headerName: 'Pflichtbereich',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'mediascience',
+            headerName: 'Wahlpflicht Informatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+        {
+            field: 'mediainfo',
+            headerName: 'Wahlpflicht Medizininformatik',
+            width: 150,
+            renderCell: (params: GridRenderCellParams) => (
+                params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }
+                } /> : <HighlightOffIcon sx={{ color: "red" }} />
+            ),
+        },
+    ]
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { createTheme, Stack, ThemeProvider, ToggleButton, ToggleButtonGroup } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import { DataGrid, GridColDef, GridRowsProp, GridToolbarFilterButton, GridToolbarQuickFilter } from '@mui/x-data-grid';
 import { deDE } from '@mui/x-data-grid/locales';
 import React from 'react';
+import { cs, FieldOfStudy, getFieldOfStudy } from './config';
 
 
 const theme = createTheme(
@@ -47,24 +46,24 @@ const columns: GridColDef[] = [
   { field: 'ects', headerName: 'ECTS', width: 150 },
   { field: 'code', headerName: 'Kennzeichnung', width: 150 },
   { field: 'lecturer', headerName: 'Dozent', width: 150 },
-  {
-    field: 'action',
-    headerName: 'Aktion',
-    width: 150,
-    renderCell: (params) => (
-      params.value ? <CheckCircleOutlineIcon sx={{ color: "green" }} /> : <HighlightOffIcon sx={{ color: "red" }} />
-    ),
-  }
 ];
 
 export default function Home() {
-  const [fieldOfStudy, setFieldOfStudy] = React.useState('test')
+  const [fieldOfStudy, setFieldOfStudy] = React.useState<FieldOfStudy>(cs);
+  const [extendedColumns, setExtendedColumns] = React.useState<GridColDef[]>([...columns, ...cs.content]);
+  console.log(extendedColumns);
+
+  React.useEffect(() => {
+    setExtendedColumns([...columns, ...fieldOfStudy.content]);
+  }, [fieldOfStudy]);
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
     newAlignment: string,
   ) => {
-    setFieldOfStudy(newAlignment);
+    console.log(newAlignment);
+    const fOS: FieldOfStudy = getFieldOfStudy(newAlignment);
+    setFieldOfStudy(fOS);
   };
 
 
@@ -80,14 +79,15 @@ export default function Home() {
               onChange={handleChange}
               aria-label="Platform"
             >
-              <ToggleButton value="web">Web</ToggleButton>
-              <ToggleButton value="android">Android</ToggleButton>
-              <ToggleButton value="ios">iOS</ToggleButton>
+              <ToggleButton value="cs">Informatik</ToggleButton>
+              <ToggleButton value="bio-cs">Bioinformatik</ToggleButton>
+              <ToggleButton value="media-cs">Medieninformatik</ToggleButton>
+              <ToggleButton value="medic-cs">Mediziniformatik</ToggleButton>
             </ToggleButtonGroup>
           </Grid>
         </Grid>
         <div style={{ width: '100%' }}>
-          <DataGrid disableColumnMenu rows={rows} columns={columns} slots={{ toolbar: Toolbar }} slotProps={{ toolbar: { showQuickFilter: true } }} />
+          <DataGrid disableColumnMenu rows={rows} columns={extendedColumns} slots={{ toolbar: Toolbar }} slotProps={{ toolbar: { showQuickFilter: true } }} />
         </div>
       </Stack>
     </ThemeProvider>
