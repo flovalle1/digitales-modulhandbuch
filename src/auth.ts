e302bf9ff1b42c2df4c48c4ec141641884a37dd7
Florian Martel <hello@florianmartel.de>
2024-11-18 12:20:11 +0100

    chore: configure next auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,34 @@
+import { prisma } from "@/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { getUserFromDb } from "./utils/auth/getUserFromDb"
+import { saltAndHashPassword } from "./utils/auth/saltAndHashPassword"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+    adapter: PrismaAdapter(prisma),
+    providers: [Credentials({
+        credentials: {
+            email: {},
+            password: {},
+        },
+        authorize: async (credentials) => {
+            let user = null
+
+            // logic to salt and hash password
+            const pwHash = await saltAndHashPassword(credentials.password as string)
+
+            // logic to verify if the user exists
+            user = await getUserFromDb(credentials.email as string, pwHash)
+
+            if (!user) {
+                // No user found, so this is their first attempt to login
+                // Optionally, this is also the place you could do a user registration
+                throw new Error("Invalid credentials.")
+            }
+
+            // return user object with their profile data
+            return user
+        },
+    }),],
 })
