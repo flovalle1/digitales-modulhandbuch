78e3e437760793a8cf9def5a451ddac9e38c3439
Florian Martel <hello@florianmartel.de>
2024-12-15 14:19:18 +0100

    fix: only fetch data when site is rendered

--- src/app/common/veranstaltungsverzeichnis/page.tsx
+++ src/app/common/veranstaltungsverzeichnis/page.tsx
@@ -3,12 +3,12 @@ import { prisma } from '@/prisma';
 import { Stack } from '@mui/material';
 
 
-const rows = await prisma.course.findMany();
 
 export const dynamic = 'force-dynamic'
-export default function Veranstaltungsverzeichnisse() {
-    return (
 
+export default async function Veranstaltungsverzeichnisse() {
+    const rows = await prisma.course.findMany();
+    return (
         <Stack spacing={5} sx={{ mx: 12, mt: 8 }}>
             {rows.map((row) => (
                 <CourseDetails key={row.id} course={row} />
