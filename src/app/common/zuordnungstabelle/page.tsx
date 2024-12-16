56ba4671228cbca98bca1ddd3d84bb4abfab5583
Florian Martel <hello@florianmartel.de>
2024-12-16 09:56:34 +0100

    feat: get db data for zuordnungstabelle

--- src/app/common/zuordnungstabelle/page.tsx
+++ src/app/common/zuordnungstabelle/page.tsx
@@ -1,54 +1,12 @@
 import AssignmentTable from "@/components/AssignmentTable";
+import { prisma } from "@/prisma";
 import { GridRowsProp } from "@mui/x-data-grid";
 import { Course } from "@prisma/client";
 
 
+export const dynamic = 'force-dynamic';
 export default async function Home(): Promise<JSX.Element> {
-
-  //const courses: Course[] = await prisma.course.findMany();
-
-  const courses: Course[] = [
-    {
-      id: 1,
-      title: "Introduction to Programming",
-      typeOfCourse: "Lecture",
-      contactTimeInHours: 30,
-      selfStudyTimeInHours: 60,
-      workloadInHours: 90,
-      teachType: "In-person",
-      ects: 3,
-      code: "CS101",
-      lastOffer: "2022-09-01",
-      nextOffer: "2023-09-01",
-      contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      qualificationGoals: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      lecturer: "Dr. John Doe",
-      requirements: "None",
-      literature: "Introduction to Programming by John Doe",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      id: 2,
-      title: "Data Structures",
-      typeOfCourse: "Lecture",
-      contactTimeInHours: 40,
-      selfStudyTimeInHours: 80,
-      workloadInHours: 120,
-      teachType: "In-person",
-      ects: 4,
-      code: "CS102",
-      lastOffer: "2022-09-01",
-      nextOffer: "2023-09-01",
-      contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      qualificationGoals: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      lecturer: "Dr. Jane Smith",
-      requirements: "Introduction to Programming",
-      literature: "Data Structures by Jane Smith",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-  ];
+  const courses: Course[] = await prisma.course.findMany();
 
   const rows: GridRowsProp = courses.map((course) => {
     return {
