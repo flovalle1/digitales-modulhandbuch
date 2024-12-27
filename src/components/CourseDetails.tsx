05cbcb713041cf9ccc03fc9272a79b2d99b3b84d
Florian Martel <hello@florianmartel.de>
2024-12-27 09:43:32 +0100

    fix: design fixes

--- src/components/CourseDetails.tsx
+++ src/components/CourseDetails.tsx
@@ -16,66 +16,68 @@ export default function CourseDetails({ course }: CourseDetailsProps) {
                     {course.code}
                 </Typography>
                 <Table>
-                    <TableRow>
-                        <TableCell >
-                            <Typography variant="body1">Arbeitsauffwand</Typography>
-                            <p>{course.workloadInHours} Stunden</p>
-                        </TableCell>
-                        <TableCell >
-                            <Typography variant="body1">Kontaktzeit</Typography>
-                            <p>{course.contactTimeInHours} Stunden</p>
-                        </TableCell>
-                        <TableCell >
-                            <Typography variant="body1">Selbststudium</Typography>
-                            <p>{course.selfStudyTimeInHours} Stunden</p>
-                        </TableCell>
-                    </TableRow>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell >
+                                <Typography variant="body1">Arbeitsaufwand</Typography>
+                                <p>{course.workloadInHours} Stunden</p>
+                            </TableCell>
+                            <TableCell >
+                                <Typography variant="body1">Kontaktzeit</Typography>
+                                <p>{course.contactTimeInHours} Stunden</p>
+                            </TableCell>
+                            <TableCell >
+                                <Typography variant="body1">Selbststudium</Typography>
+                                <p>{course.selfStudyTimeInHours} Stunden</p>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
                 </Table>
                 <Table>
                     <TableBody>
                         <TableRow>
                             <TableCell>ECTS</TableCell>
-                            <TableCell colSpan={2}>{course.ects}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.ects}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Veranstaltungsdauer</TableCell>
-                            <TableCell colSpan={2}>1 Semester</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>1 Semester</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Häufigkeit des Angebots</TableCell>
-                            <TableCell colSpan={2}>Jährlich / Wintersemester</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>Jährlich / Wintersemester</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Prüfungsform</TableCell>
-                            <TableCell colSpan={2}>Klausur</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>Klausur</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Inhalt</TableCell>
-                            <TableCell colSpan={2}>{course.contents}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.contents}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Qualifikationsziele</TableCell>
-                            <TableCell colSpan={2}>{course.qualificationGoals}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.qualificationGoals}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Teilnahmevoraussetzungen</TableCell>
-                            <TableCell colSpan={2}>{course.requirements}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.requirements}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Dozent/in</TableCell>
-                            <TableCell colSpan={2}>{course.lecturer}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.lecturer}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Literatur</TableCell>
-                            <TableCell colSpan={2}>{course.literature}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.literature}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Zuletzt Angeboten</TableCell>
-                            <TableCell colSpan={2}>{course.lastOffer}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.lastOffer}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Geplant für</TableCell>
-                            <TableCell colSpan={2}>{course.nextOffer}</TableCell>
+                            <TableCell sx={{ minWidth: 1000 }} colSpan={2}>{course.nextOffer}</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
