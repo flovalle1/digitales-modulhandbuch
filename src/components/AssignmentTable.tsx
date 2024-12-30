4cd1b0af48da95c28b01717586f2988624958cc0
Florian Martel <hello@florianmartel.de>
2024-12-30 10:21:52 +0100

    chore: enable data sync betwenn components

--- src/components/AssignmentTable.tsx
+++ src/components/AssignmentTable.tsx
@@ -1,9 +1,8 @@
 "use client";
 import FilterListIcon from '@mui/icons-material/FilterList';
-import { Card, Stack, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import Fab from '@mui/material/Fab';
-import Grid from '@mui/material/Grid2';
-import { DataGrid, GridColDef, GridRowsProp, GridToolbarFilterButton, GridToolbarQuickFilter } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 import React from 'react';
 import { cs, FieldOfStudy, getFieldOfStudy } from './config';
 import TableDrawer from './TableDrawer';
@@ -13,23 +12,6 @@ import TableDrawer from './TableDrawer';
 export interface AssignmentTableProps {
     rows: GridRowsProp;
 }
-
-function Toolbar() {
-    return (
-        <div>
-            <GridToolbarFilterButton />
-            <GridToolbarQuickFilter
-                quickFilterParser={(searchInput: string) =>
-                    searchInput
-                        .split(',')
-                        .map((value) => value.trim())
-                        .filter((value) => value !== '')
-                }
-            />
-        </div>
-    );
-}
-
 
 const columns: GridColDef[] = [
     { field: 'titel', headerName: 'Titel', width: 150 },
@@ -47,21 +29,27 @@ export default function AssignmentTable({ rows }: AssignmentTableProps) {
     const [fieldOfStudy, setFieldOfStudy] = React.useState<FieldOfStudy>(cs);
     const [extendedColumns, setExtendedColumns] = React.useState<GridColDef[]>([...columns, ...cs.content]);
     const [drawerOpen, setDrawerOpen] = React.useState(false);
-
     const [filterOption, setFilterOption] = React.useState<filterOption>({ key: 'cs' })
 
     React.useEffect(() => {
         setExtendedColumns([...columns, ...fieldOfStudy.content]);
     }, [fieldOfStudy]);
 
-    const handleChange = (
-        event: React.MouseEvent<HTMLElement>,
-        newAlignment: string,
-    ) => {
-        setFilterOption({ key: newAlignment });
-        const fOS: FieldOfStudy = getFieldOfStudy(newAlignment);
-        setFieldOfStudy(fOS);
-    };
+    React.useEffect(() => {
+        setFieldOfStudy(getFieldOfStudy(filterOption.key));
+    }, [filterOption]);
+
+    function Toolbar() {
+        return (
+            <Box display="flex" justifyContent="flex-start" alignItems="flex-start">
+                <Button
+                    onClick={() => setDrawerOpen(true)}
+                >
+                    Filter
+                </Button>
+            </Box>
+        );
+    }
 
     return (
         <Stack sx={{ m: 4, p: 2 }} >
@@ -72,45 +60,8 @@ export default function AssignmentTable({ rows }: AssignmentTableProps) {
                 changeFilter={setFilterOption}
             />
 
-            <Grid container>
-                <Grid size={5} >
-                    <Card >
-                        <Typography variant="h6" align="center">Bachelor Studiengänge</Typography>
-                        <ToggleButtonGroup
-                            color="primary"
-                            value={filterOption.key}
-                            exclusive
-                            onChange={handleChange}
-                            aria-label="Platform"
-                            sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}
-                        >
-                            <ToggleButton value="cs">Informatik</ToggleButton>
-                            <ToggleButton value="bio-cs">Bioinformatik</ToggleButton>
-                            <ToggleButton value="media-cs">Medieninformatik</ToggleButton>
-                            <ToggleButton value="medic-cs">Mediziniformatik</ToggleButton>
-                        </ToggleButtonGroup>
-                        <Typography variant="h6" align="center">Master Studiengänge</Typography>
-                        <ToggleButtonGroup
-                            color="primary"
-                            value={filterOption.key}
-                            exclusive
-                            onChange={handleChange}
-                            aria-label="Platform"
-                            sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}
-                        >
-                            <ToggleButton value="cs">Informatik</ToggleButton>
-                            <ToggleButton value="bio-cs">Bioinformatik</ToggleButton>
-                            <ToggleButton value="media-cs">Medieninformatik</ToggleButton>
-                            <ToggleButton value="medic-cs">Mediziniformatik</ToggleButton>
-                        </ToggleButtonGroup>
-                    </Card>
-                </Grid>
-            </Grid>
-
-
-
             <div style={{ width: '100%' }}>
-                <DataGrid disableColumnMenu rows={rows} columns={extendedColumns} slots={{ toolbar: Toolbar }} slotProps={{ toolbar: { showQuickFilter: true } }} />
+                <DataGrid disableColumnMenu rows={rows} columns={extendedColumns} slots={{ toolbar: Toolbar }} />
             </div>
 
             <Fab
--- src/components/TableDrawer.tsx
+++ src/components/TableDrawer.tsx
@@ -1,7 +1,30 @@
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Card, Drawer, IconButton, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import { Box, Card, Drawer, IconButton, Stack, Tab, Tabs, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import React from 'react';
 import { filterOption } from './AssignmentTable';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
+}
 
 export interface TableDrawerProps {
     open: boolean;
@@ -11,6 +34,12 @@ export interface TableDrawerProps {
 }
 
 export default function TableDrawer({ open, onClose, filters, changeFilter }: TableDrawerProps) {
+    const [tabValue, setTabValue] = React.useState(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
+    };
+
     const handleFieldChange = (
         event: React.MouseEvent<HTMLElement>,
         newAlignment: string,
@@ -34,42 +63,52 @@ export default function TableDrawer({ open, onClose, filters, changeFilter }: Ta
         >
             <Stack spacing={2}>
                 <Stack direction="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h6">Filter Options</Typography>
+                    <Typography variant="h5">Filter</Typography>
                     <IconButton onClick={onClose}>
                         <ChevronLeftIcon />
                     </IconButton>
                 </Stack>
 
                 <Card sx={{ p: 2 }}>
-                    <Typography variant="h6" align="center" gutterBottom>Bachelor Studiengänge</Typography>
-                    <ToggleButtonGroup
-                        color="primary"
-                        value={filters.key}
-                        exclusive
-                        onChange={handleFieldChange}
-                        orientation="vertical"
-                        sx={{ width: '100%', mb: 2 }}
-                    >
-                        <ToggleButton value="cs">Informatik</ToggleButton>
-                        <ToggleButton value="bio-cs">Bioinformatik</ToggleButton>
-                        <ToggleButton value="media-cs">Medieninformatik</ToggleButton>
-                        <ToggleButton value="medic-cs">Mediziniformatik</ToggleButton>
-                    </ToggleButtonGroup>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider', justifyContent: 'center', display: 'flex' }}>
+                        <Tabs value={tabValue} onChange={handleTabChange}>
+                            <Tab label="Bachelor" />
+                            <Tab label="Master" />
+                        </Tabs>
+                    </Box>
 
-                    <Typography variant="h6" align="center" gutterBottom>Master Studiengänge</Typography>
-                    <ToggleButtonGroup
-                        color="primary"
-                        value={filters.key}
-                        exclusive
-                        onChange={handleFieldChange}
-                        orientation="vertical"
-                        sx={{ width: '100%' }}
-                    >
-                        <ToggleButton value="cs-m">Informatik</ToggleButton>
-                        <ToggleButton value="bio-cs-m">Bioinformatik</ToggleButton>
-                        <ToggleButton value="media-cs-m">Medieninformatik</ToggleButton>
-                        <ToggleButton value="medic-cs-m">Mediziniformatik</ToggleButton>
-                    </ToggleButtonGroup>
+                    <CustomTabPanel value={tabValue} index={0}>
+                        <ToggleButtonGroup
+                            color="primary"
+                            value={filters.key}
+                            exclusive
+                            onChange={handleFieldChange}
+                            orientation="vertical"
+                            sx={{ width: '100%' }}
+                        >
+                            <ToggleButton value="cs">Informatik</ToggleButton>
+                            <ToggleButton value="bio-cs">Bioinformatik</ToggleButton>
+                            <ToggleButton value="media-cs">Medieninformatik</ToggleButton>
+                            <ToggleButton value="medic-cs">Medizininformatik</ToggleButton>
+                        </ToggleButtonGroup>
+                    </CustomTabPanel>
+
+                    <CustomTabPanel value={tabValue} index={1}>
+                        <ToggleButtonGroup
+                            color="primary"
+                            value={filters.key}
+                            exclusive
+                            onChange={handleFieldChange}
+                            orientation="vertical"
+                            sx={{ width: '100%' }}
+                        >
+                            <ToggleButton value="cs-master">Informatik</ToggleButton>
+                            <ToggleButton value="bio-cs-master">Bioinformatik</ToggleButton>
+                            <ToggleButton value="media-cs-master">Medieninformatik</ToggleButton>
+                            <ToggleButton value="medic-cs-master">Medizininformatik</ToggleButton>
+                            <ToggleButton value="machine-learning-master">Machine Learning</ToggleButton>
+                        </ToggleButtonGroup>
+                    </CustomTabPanel>
                 </Card>
             </Stack>
         </Drawer>
