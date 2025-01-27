62290b9958f90bf3be95bdc57956a02586b570f0
Florian Martel <hello@florianmartel.de>
2025-01-27 15:28:54 +0100

    feat: create CourseDetailPage

---
+++ src/app/common/veranstaltungsverzeichnis/[courseId]/page.tsx
@@ -0,0 +1,26 @@
+import CourseDetails from '@/components/CourseDetails';
+import { prisma } from '@/prisma';
+import { Stack } from '@mui/material';
+
+export const dynamic = 'force-dynamic';
+
+export interface PageProps {
+    params: Promise<{ courseId: string }>;
+}
+
+export default async function CourseDetailPage({ params }: PageProps) {
+    const { courseId } = await params;
+    const row = await prisma.course.findFirst({
+        where: {
+            id: Number(courseId),
+        },
+        include: {
+            lecturer: true,
+        },
+    });
+    return (
+        <Stack spacing={5} sx={{ mx: 12, mt: 8 }}>
+            {row && <CourseDetails key={row.id} course={row} />}
+        </Stack>
+    );
+}
