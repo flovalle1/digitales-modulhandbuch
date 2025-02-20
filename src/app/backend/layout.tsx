3a335e7002b0d1b9d988b77ad15d324a62c976b3
Florian Martel <hello@florianmartel.de>
2025-02-20 10:34:56 +0100

    fix: add width and hight to Image Component

--- src/app/backend/layout.tsx
+++ src/app/backend/layout.tsx
@@ -95,7 +95,7 @@ export default function AdminLayout({
             <AppProvider
                 navigation={NAVIGATION}
                 theme={theme}
-                branding={{ title: 'Dozenten Backend', logo: <Image src="/Universitaet_Tuebingen.png" alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} /> }}
+                branding={{ title: 'Dozenten Backend', logo: <Image src="/Universitaet_Tuebingen.png" alt="Logo" width={150} height={150} style={{ maxWidth: '100%', height: 'auto' }} /> }}
             >
                 {children}
             </AppProvider>
