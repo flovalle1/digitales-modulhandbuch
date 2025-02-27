06ba5c30ba1564093e6cf231a195fb692d100624
Florian Martel <hello@florianmartel.de>
2025-02-27 13:40:36 +0100

    fix: force dynamic render

--- src/app/common/layout.tsx
+++ src/app/common/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
   description: "Digitales Modulhandbuch Informatik Universität Tübingen.",
 };
 
+export const dynamic = 'force-dynamic'
+
 export default async function RootLayout({
   children,
 }: Readonly<{
